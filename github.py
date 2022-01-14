66cf675dc93abba466f6d7ebde1d87f25dd68fa6
Haris Anwar <>
2022-01-15 01:46:23 +0500

    feat(star timestamp info): Getting star timestamp info of provided github repository

--- github.py
+++ github.py
@@ -1,5 +1,17 @@
-import requests
+"""
+info: github util functions
+@author: Haris Anwar <harisanwar64@gmail.com>
+"""
 
+import requests
+from math import ceil
+import pandas as pd
+
+
+def get_header():
+    header = {'Accept': 'application/vnd.github.v3.star+json',
+              'Authorization': "Token " + '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'}
+    return header
 
 class GitService(object):
 
@@ -7,8 +19,7 @@ class GitService(object):
         pass
 
     def check_repo_exists(self, repo):
-        header = {'Accept': 'application/vnd.github.v3.star+json',
-                  'Authorization': "Token " + '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'}
+        header = get_header()
         request_url = f"https://api.github.com/repos/{repo}"
         request_result = requests.get(request_url, headers=header)
         status_code = request_result.status_code
@@ -18,14 +29,30 @@ class GitService(object):
             return False
 
     def get_repo_star_count(self, repo):
-        header = {'Accept': 'application/vnd.github.v3.star+json',
-                  'Authorization': "Token " + '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'}
+        header = get_header()
         request_url = f"https://api.github.com/repos/{repo}"
         request_result = requests.get(request_url, headers=header).json()
         return request_result['stargazers_count']
 
+    def get_repo_total_pages(self, repo):
+        per_page = 100
+        star_count = self.get_repo_star_count(repo)
+        required_pages = ceil(star_count / per_page)
+        return required_pages
+
+    def get_repo_star_info_list(self, repo):
+        total_pages = self.get_repo_total_pages(repo)
+        header = get_header()
+        star_info_list = []
+        for page in range(1, total_pages):
+            request_url = f"https://api.github.com/repos/{repo}/stargazers?per_page=100&page={page}"
+            request_result = requests.get(request_url, headers=header).json()
+            star_info_list.append(request_result)
+        return star_info_list
+
 
 if __name__ == "__main__":
+    #todo: need to input repo from user (ui)
     repo = 'pytorch/kineto'
-    # repo_exists = GitService().check_repo_exists(repo)
-    star_count = GitService().get_repo_star_count(repo)
+    star_list = GitService().get_repo_star_info_list(repo)
+    dataframe = pd.DataFrame(star_list)
