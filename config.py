ce08b85a6b5f3f62cafa797702cb906c03cf8111
Haris Anwar <>
2022-01-15 13:50:15 +0500

    test: unit test added in directory 'tests'

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class GitConfig:
     DEBUG = False
     GIT_API_URL = "https://api.github.com"
     GIT_USERNAME = 'harisanwar64'
-    GIT_API_TOKEN = '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'
+    GIT_API_TOKEN = '******************'
     # version required for timestamp information of star
     GIT_DEV_VERSION = 'application/vnd.github.v3.star+json'
 
---
+++ tests/unit_test.py
@@ -0,0 +1,20 @@
+import unittest
+import requests
+from config import GitConfig
+
+
+class UnitTest(unittest.TestCase):
+
+    def test_github_config(self):
+        """this function is created to test github configuration e.g. github api server (url) is up and running,
+        header is correct, access token is valid and correct etc."""
+        header = {'Accept': GitConfig.GIT_DEV_VERSION, 'Authorization': "Token " + GitConfig.GIT_API_TOKEN}
+        request_url = f"{GitConfig.GIT_API_URL}"
+        request_result = requests.get(request_url, headers=header)
+        self.assertEqual(request_result.status_code, 200)
+
+
+if __name__ == '__main__':
+    # todo: more tests can be added for star info in dataframe and graph, csv created in csv_downloads or not,
+    #  flask api responses etc.
+    unittest.main()
